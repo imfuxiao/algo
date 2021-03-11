a2bc9a3a1d9249bd22f2048e006dd57a97cbb387
fu.xiao <im.fu.xiao@gmail.com>
2021-03-11 12:33:05 +0800

    add leetcode calculate

--- leetcode/string/calculate.go
+++ leetcode/string/calculate.go
@@ -1,5 +1,8 @@
 package string
 
+import "math"
+
+// 224. 基本计算器
 func calculate(s string) int {
 	var (
 		length = len(s)
@@ -32,3 +35,132 @@ func calculate(s string) int {
 	}
 	return ans
 }
+
+// 227. 基本计算器 II
+func calculate2(s string) int {
+	var (
+		length = len(s)
+		stack  = make([]int, 0, length)
+		sign   = '+'
+	)
+
+	for i := 0; i < length; i++ {
+		switch s[i] {
+		case ' ':
+		case '+':
+			sign = '+'
+		case '-':
+			sign = '-'
+		case '*':
+			sign = '*'
+		case '/':
+			sign = '/'
+		default:
+			num := 0
+			for i < length && '0' <= s[i] && s[i] <= '9' {
+				num = num*10 + int(s[i]-'0')
+				i++
+			}
+			i--
+			switch sign {
+			case '+':
+				stack = append(stack, num)
+			case '-':
+				stack = append(stack, -num)
+			case '*':
+				stack = append(stack[:len(stack)-1], stack[len(stack)-1]*num)
+			case '/':
+				stack = append(stack[:len(stack)-1], stack[len(stack)-1]/num)
+			}
+		}
+	}
+
+	ans := 0
+	for i := range stack {
+		ans += stack[i]
+	}
+
+	if ans > math.MaxInt32 {
+		return math.MaxInt32
+	}
+
+	return ans
+}
+
+// 772. 基本计算器 III
+func calculate3(s string) int {
+	var (
+		length                = len(s)
+		sign             byte = '+'
+		stack                 = make([]int, 0, length)
+		parentheses           = make([]byte, 0, length)
+		parenthesesIndex      = make([]int, 0, length)
+	)
+
+	for i := 0; i < length; i++ {
+		switch s[i] {
+		case ' ':
+		case '+':
+			sign = '+'
+		case '-':
+			sign = '-'
+		case '*':
+			sign = '*'
+		case '/':
+			sign = '/'
+		case '(':
+			parentheses = append(parentheses, sign)
+			parenthesesIndex = append(parenthesesIndex, len(stack))
+			sign = '+'
+		case ')':
+			sign = parentheses[len(parentheses)-1]
+			parentheses = parentheses[:len(parentheses)-1]
+			sum := 0
+			start := parenthesesIndex[len(parenthesesIndex)-1]
+			parenthesesIndex = parenthesesIndex[:len(parenthesesIndex)-1]
+			for j := start; j < len(stack); j++ {
+				sum += stack[j]
+			}
+			stack = stack[:start]
+			switch sign {
+			case '+':
+				stack = append(stack, sum)
+			case '-':
+				stack = append(stack, -sum)
+			case '*':
+				stack = append(stack[:len(stack)-1], stack[len(stack)-1]*sum)
+			case '/':
+				stack = append(stack[:len(stack)-1], stack[len(stack)-1]/sum)
+			}
+		default:
+			num := 0
+			for i < length && s[i] >= '0' && s[i] <= '9' {
+				num = num*10 + int(s[i]-'0')
+				i++
+			}
+			i--
+
+			switch sign {
+			case '+':
+				stack = append(stack, num)
+			case '-':
+				stack = append(stack, -num)
+			case '*':
+				stack = append(stack[:len(stack)-1], stack[len(stack)-1]*num)
+			case '/':
+				stack = append(stack[:len(stack)-1], stack[len(stack)-1]/num)
+			}
+		}
+	}
+
+	ans := 0
+	for i := range stack {
+		ans += stack[i]
+	}
+	if ans > math.MaxInt32 {
+		return math.MaxInt32
+	}
+	return ans
+}
+
+// 770. 基本计算器 IV
--- leetcode/string/calculate_test.go
+++ leetcode/string/calculate_test.go
@@ -50,3 +50,91 @@ func Test_calculate(t *testing.T) {
 		})
 	}
 }
+
+func Test_calculate2(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "1-10+1",
+			args: args{s: "1-10+1"},
+			want: -8,
+		},
+		{
+			name: "3+2*2",
+			args: args{s: "3+2*2"},
+			want: 7,
+		},
+		{
+			name: " 3/2 ",
+			args: args{s: " 3/2 "},
+			want: 1,
+		},
+		{
+			name: " 3+5 / 2 ",
+			args: args{s: " 3+5 / 2 "},
+			want: 5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := calculate2(tt.args.s); got != tt.want {
+				t.Errorf("calculate2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_calculate3(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "0",
+			args: args{s: "0"},
+			want: 0,
+		},
+		{
+			name: "11",
+			args: args{s: "11"},
+			want: 11,
+		},
+		{
+			name: "1+1",
+			args: args{s: "1+1"},
+			want: 2,
+		},
+		{
+			name: "6-4/2",
+			args: args{s: "6-4/2"},
+			want: 4,
+		},
+		{
+			name: "2*(5+5*2)/3+(6/2+8)",
+			args: args{s: "2*(5+5*2)/3+(6/2+8)"},
+			want: 21,
+		},
+		{
+			name: "(2+6*3+5-(3*14/7+2)*5)+3",
+			args: args{s: "(2+6*3+5-(3*14/7+2)*5)+3"},
+			want: -12,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := calculate3(tt.args.s); got != tt.want {
+				t.Errorf("calculate3() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
